93334d755c53e64abe496608a613e1f5f9308e48
Dani Fernandez <daniel.fx.90@gmail.com>
2014-04-03 15:23:36 -0300

    Me falto subir la exception.

---
+++ simulador/src/main/java/ar/com/compumundohipermegared/simulador/DivisionByZeroException.java
@@ -0,0 +1,10 @@
+package ar.com.compumundohipermegared.simulador;
+
+public class DivisionByZeroException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
