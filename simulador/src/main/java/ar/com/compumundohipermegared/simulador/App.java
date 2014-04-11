4d99ff5a9c91af9a6ab3e681e104c7852c73e3a7
administrador <administrador@administrador-VirtualBox.(none)>
2014-04-11 11:49:24 -0300

    Parser Instrucciones

--- simulador/src/main/java/ar/com/compumundohipermegared/simulador/App.java
+++ simulador/src/main/java/ar/com/compumundohipermegared/simulador/App.java
@@ -1,5 +1,10 @@
 package ar.com.compumundohipermegared.simulador;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import ar.com.compumundohipermegared.almacenamiento.ParserInstrucciones;
+
 /**
  * Hello world!
  *
