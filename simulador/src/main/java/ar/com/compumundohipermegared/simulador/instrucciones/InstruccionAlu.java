e25d85a1edfac36ab096f6ac8dbd801643c12abf
administrador <administrador@administrador-VirtualBox.(none)>
2014-05-01 12:43:53 -0300

    Instruccion Copiar

--- simulador/src/main/java/ar/com/compumundohipermegared/simulador/instrucciones/InstruccionAlu.java
+++ simulador/src/main/java/ar/com/compumundohipermegared/simulador/instrucciones/InstruccionAlu.java
@@ -1,5 +1,6 @@
 package ar.com.compumundohipermegared.simulador.instrucciones;
 
+import ar.com.compumundohipermegared.simulador.cicloInstruccion.Cpu;
 import ar.com.compumundohipermegared.simulador.cicloInstruccion.Parametros;
 
 abstract class InstruccionAlu extends Instruccion{
@@ -8,4 +9,11 @@ abstract class InstruccionAlu extends Instruccion{
 		super(parametros);
 	}
 
+
+	protected Cpu cpu = null;
+	
+	@Override
+	public void cargarOperandos(Cpu cpuRecibida) {
+		cpu = cpuRecibida;
+	}
 }
--- simulador/src/main/java/ar/com/compumundohipermegared/simulador/instrucciones/InstruccionCopiar.java
+++ simulador/src/main/java/ar/com/compumundohipermegared/simulador/instrucciones/InstruccionCopiar.java
@@ -4,18 +4,24 @@ import ar.com.compumundohipermegared.simulador.cicloInstruccion.Cpu;
 import ar.com.compumundohipermegared.simulador.cicloInstruccion.Parametros;
 
 public class InstruccionCopiar extends InstruccionAlu {
-
+	byte dato;
+	int idRegistroDestino;
 	public InstruccionCopiar(Parametros parametros) {
 		super(parametros);
 	}
 
 	@Override
 	public void ejecutar() {
+		cpu.EscribirRegistro(idRegistroDestino, dato);
 		System.out.println("Ejecutando una instruccion de copiar");		
 	}
 
 	@Override
 	public void cargarOperandos(Cpu cpuRecibida) {
+		super.cargarOperandos(cpuRecibida);
+		int idFuente = _parametros.getSegundoParametro();
+		dato = cpu.ObtenerRegsitro(idFuente);
+		idRegistroDestino = _parametros.getTercerParametro();
 		System.out.println("Cargando operandos una instruccion de copiar");		
 	}
 
