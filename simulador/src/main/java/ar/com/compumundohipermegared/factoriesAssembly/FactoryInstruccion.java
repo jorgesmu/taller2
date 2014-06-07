3015635399cad7a347ad4ce54f075ca247805376
Dani Fernandez <daniel.fx.90@gmail.com>
2014-06-07 16:37:04 -0300

    Constructor Factory

--- simulador/src/main/java/ar/com/compumundohipermegared/factoriesAssembly/FactoryInstruccion.java
+++ simulador/src/main/java/ar/com/compumundohipermegared/factoriesAssembly/FactoryInstruccion.java
@@ -1,5 +1,15 @@
 package ar.com.compumundohipermegared.factoriesAssembly;
 
-public class FactoryInstruccion {
+import java.util.Map;
 
+public class FactoryInstruccion {
+	
+	Map<String, String> misLabels;
+	Map<String, String> misOperandos;
+	
+	public FactoryInstruccion (Map<String, String> labels, Map<String, String> operandos) {
+		misLabels = labels;
+		misOperandos = operandos;
+	}
+	
 }
